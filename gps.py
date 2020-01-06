60264a8081d6fa2265e069700d6306603450e496
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-06 21:03:27 +0100

    Update gps.py

--- gps.py
+++ gps.py
@@ -5,4 +5,7 @@ gps = serial.Serial("/dev/ttyUSB0", baudrate=4800)
 for i in range(0,10):
     line = gps.readline()
     data_line = line.decode()
-    print(data_line)
+    if data_line[0] == '$GPZDA':
+        print('time: {}'.format(data_line))
+    else:
+        pass
