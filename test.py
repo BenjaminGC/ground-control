f52ea6bfdc5a34e5456fd43d2b4cb473fb0e102a
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 00:39:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,19 +6,17 @@ conv_f = 1.852
 
 
 def gps_speed(data):
-    line = str(data.readline(), 'ASCII')
-    data_line = line.split(',')
-    if data_line[0] == '$GPRMC':
-        speed = float(data_line[7])*conv_f
-        print("Speed: {} km/h".format(speed))
-        return speed
+    with open('speed.txt', 'w') as file:
+        line = str(data.readline(), 'ASCII')
+        data_line = line.split(',')
+        if data_line[0] == '$GPRMC':
+            speed = float(data_line[7])*conv_f
+            print("Speed: {} km/h".format(speed))
+            file.write(speed)
 
 
 while status:
-    with open("speed.txt", "w") as v_t:
-        try:
-            speed = gps_speed(gps)
-            v_t.write(speed)
-        except UnicodeDecodeError:
-            speed = gps_speed(gps)
-            v_t.write(speed)
+    try:
+        gps_speed(gps)
+    except UnicodeDecodeError:
+        gps_speed(gps)
