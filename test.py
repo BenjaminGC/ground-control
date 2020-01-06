884e9aebd45323f12aababd391e29c49416b7227
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 00:58:56 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,27 @@
 import serial
+import csv
 
 gps = serial.Serial("/dev/ttyUSB0", baudrate=4800, timeout=5)
 status = True
 conv_f = 1.852
 
 
-def gps_speed(data, index):
-    with open('speed.txt', 'w') as file:
-        line = str(data.readline(), 'ASCII')
-        data_line = line.split(',')
-        if data_line[0] == '$GPRMC':
-            speed = float(data_line[7])*conv_f
-            if speed <= 1.0:
-                speed = 0
-            else:
-                pass
-            print("Speed: {} km/h".format(speed))
-            file.write("{}, {}".format(i, str(speed)))
+def gps_speed(data):
+    line = str(data.readline(), 'ASCII')
+    data_line = line.split(',')
+    if data_line[0] == '$GPRMC':
+        speed = float(data_line[7])*conv_f
+        if speed <= 1.0:
+            speed = 0
+        else:
+            pass
+        return speed
 
 
-for i in range(0, 25):
-    try:
-        gps_speed(gps, i)
-    except UnicodeDecodeError:
-        gps_speed(gps, i)
+with open('speed.csv', 'w', newline='') as file:
+    file_writer = csv.writer(file)
+    for i in range(0, 25):
+        try:
+            file_writer.writerow(gps_speed(gps))
+        except UnicodeDecodeError:
+            file_writer.writerow(gps_speed(gps))
