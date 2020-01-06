5bdd72e74897e57ba137b642bc44a8bf8e47c3d5
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 00:10:36 +0100

    Update gps_loc.py

--- gps_loc.py
+++ gps_loc.py
@@ -4,38 +4,30 @@ gps = serial.Serial("/dev/ttyUSB0", baudrate=4800, timeout=5)
 status = True
 
 
-def degrees(pos):
-    split_pos = pos.split('.')
-    seconds = str((int(split_pos[1]) / pow(10, len(split_pos[1]))) * 60)
-    if len(split_pos[0]) == 4:
-        minutes = str(split_pos[0][2:])
-        hours = str(split_pos[0][0:2])
-        location = hours + "°" + minutes + "'" + seconds + '"'
-        return location
-    elif len(split_pos[0]) == 5:
-        minutes = str(split_pos[0][3:])
-        hours = str(split_pos[0][0:3])
-        location = hours + "°" + minutes + "'" + seconds + '"'
-        return location
+def gps_loc(gps):
+    line = gps.readline()
+    data = line.split(",")
+    if data[0] == '$GPRMC':
+        if data[2] == 'A':
 
-
-def gps_loc(data):
-    line = str(data.readline(), 'ASCII')
-    data_line = line.split(',')
-    if data_line[0] == '$GPGGA':
-        local_time = data_line[1][:6]
-        local_time = str("{}:{}:{}".format(local_time[0:2], local_time[2:4], local_time[4:6]))
-        lat = str(degrees(data_line[2]) + data_line[3])
-        lon = str(degrees(data_line[4]) + data_line[5])
-        altitude = data_line[9]
-        satellites_in_use = data_line[7]
-        print('Local Time (SH): {} Z, Latitude: {}, Longitude: {}, Altitude (MSL): {}'.format(local_time
-                                                                                              , lat
-                                                                                              , lon
-                                                                                              , altitude))
-        print('Satellites in use: {}'.format(satellites_in_use))
-    else:
-        pass
+            lat_gps = float(data[3])
+            if data[4] == "S":
+                lat_gps = -lat_gps
+            
+            lat_deg = int(lat_gps/100)
+            lat_min = lat_gps - lat_gps*100
+            lat = lat_deg + (lat_min/60)
+            
+            long_gps = float(data[5])
+            if data[4] == "W":
+                long_gps = - long_gps
+            
+            long_deg = int(long_gps/100)
+            long_min = long_gps - long_deg*100
+            lon = long_deg + (long_min/60)
+            
+            print("lat: {}".format(lat))
+            print("lon: {}".format(lon))
 
 
 while status:
