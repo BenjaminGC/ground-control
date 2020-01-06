013b8fcb9cb4a4a3d016345ce040a5eae7da3bb4
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-06 21:21:48 +0100

    Update gps.py

--- gps.py
+++ gps.py
@@ -2,7 +2,10 @@ import serial
 
 gps = serial.Serial("/dev/ttyUSB0", baudrate=4800)
 
-for i in range(0,10):
+for i in range(0, 20):
     line = str(gps.readline(), 'ASCII')
     data_line = line.split(',')
-    print(data_line)
+    if data_line[0] == '$GPGSV':
+        print('Satellites in view: {}'.format(data_line))
+    else:
+        pass
