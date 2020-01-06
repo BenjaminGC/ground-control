8e550094f8c23424163ae3098afe940dbd69455b
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-06 21:48:26 +0100

    Update gps.py

--- gps.py
+++ gps.py
@@ -6,15 +6,7 @@ for i in range(0, 20):
     line = str(gps.readline())
     data_line = line.split(',')
     if data_line[0] == '$GPGGA':
-        print('Local Time: {} Z, Latitude: {} {}, Longitude: {} {}, Altitude: {}'.format(data_line[1], data_line[2], data_line[3], 
-                                                                                    data_line[4], data_line[5],
-                                                                                    data_line[9]
-                                                                                   )
-             )
-        print('\n')
+        print('Local Time: {} Z, Latitude: {} {}, Longitude: {} {}, Altitude: {} \n'.format(data_line[1], data_line[2], data_line[3], data_line[4], data_line[5], data_line[9]))
         print('Satellites in use: {}'.format(data_line[7]))
-    # if data_line[0] == '$GPGSV':
-    #    print('Satellites in view: {}'.format(data_line[3])
-    #    print('Current sattelite data:
     else:
         pass
