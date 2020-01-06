7af2247e8b191dfd49af01232dc1da9ca35245b0
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-06 20:59:36 +0100

    Update gps.py

--- gps.py
+++ gps.py
@@ -3,5 +3,6 @@ import serial
 gps = serial.Serial("/dev/ttyUSB0", baudrate=4800)
 
 for i in range(0,10):
-    line = str(gps.readline(), 'utf-8')
-    print(line)
+    line = gps.readline()
+    data_line = line.decode()
+    print(data_line)
