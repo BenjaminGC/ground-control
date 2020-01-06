c692e51157422f8ad23bdb9eb7e4ac2d94dcba45
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-06 21:06:35 +0100

    Update gps.py

--- gps.py
+++ gps.py
@@ -2,10 +2,10 @@ import serial
 
 gps = serial.Serial("/dev/ttyUSB0", baudrate=4800)
 
-for i in range(0,10):
+for i in range(0,100):
     line = gps.readline()
     data_line = line.decode()
-    if data_line[0] == '$GPZDA':
-        print('time: {}'.format(data_line))
+    if data_line[0] == '$GPGSV':
+        print('satellites in view: {}'.format(data_line))
     else:
         pass
