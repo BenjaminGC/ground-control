683aecfe4e5bec09a82bad027670e4f9f6ab3840
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-06 21:49:08 +0100

    Update gps.py

--- gps.py
+++ gps.py
@@ -3,7 +3,7 @@ import serial
 gps = serial.Serial("/dev/ttyUSB0", baudrate=4800)
 
 for i in range(0, 20):
-    line = str(gps.readline())
+    line = str(gps.readline(), 'ASCII')
     data_line = line.split(',')
     if data_line[0] == '$GPGGA':
         print('Local Time: {} Z, Latitude: {} {}, Longitude: {} {}, Altitude: {} \n'.format(data_line[1], data_line[2], data_line[3], data_line[4], data_line[5], data_line[9]))
