0ba74b839c2539a762e64a484b0414e1b182c924
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 16:32:53 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,13 +1,14 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+def button_callback(channel):
+    print("Button was pushed!")
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 13 to be an input pin and set initial value to be pulled low (off)
 
-try:
-    while True: # Run forever
-        if GPIO.input(13) == GPIO.HIGH:
-            print("Button was pushed!")
-except KeyboardInterrupt:
-    GPIO.cleanup()
+GPIO.add_event_detect(13,GPIO.RISING,callback=button_callback) # Setup event on pin 13 rising edge
 
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+
+GPIO.cleanup() # Clean up
