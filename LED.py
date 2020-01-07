b8c8b681498dfc74c8d20cf3259a4838506690f3
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 01:57:39 +0100

    Add files via upload

---
+++ LED.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+for x in range(0,3):
+    GPIO.output(7, True)
+    time.sleep(1)
+    GPIO.output(7, False)
+    time.sleep(1)
+GPIO.cleanup()
