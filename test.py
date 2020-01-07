7864fd17d498f40ad1939cd83425290cedd62e07
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 15:10:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -9,18 +9,19 @@ DEFAULT_VALUE = 0.0
 index = int(0)
 
 
-def gps_speed(data):
+def gps_speed(data, index):
     line = str(data.readline(), 'ASCII')
     data_line = line.split(',')
     if data_line[0] == '$GPRMC':
         speed = data_line[7]
         print("{}:  {} km/h".format(index, speed))
-        index += 1
         time.sleep(1)
 
         
 while status:
     try:
         gps_speed(gps)
+        index += 1
     except UnicodeDecodeError:
         gps_speed(gps)
+        index += 1
