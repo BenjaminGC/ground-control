883db1d0a52210dc16ef5725493dab8a2b78515d
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 16:23:41 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(13, GPIO.IN)
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 try:
     while True: # Run forever
