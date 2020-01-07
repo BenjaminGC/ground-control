ca463282090a818636228da744e5f1efb2de6489
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 02:29:56 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,16 +1,15 @@
 import RPi.GPIO as GPIO
 
-
-ButtonPin = 13
-
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(ButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 try:
     while True:
-        if GPIO.input(11) == 1:
+        if GPIO.input(13) == 1:
             print(True)
         else:
             print(False)
 except KeyboardInterrupt:
     GPIO.cleanup()
+
