fcf31dc72e0633cccfbcac311be0f500b5f0048b
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 02:51:06 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(13, GPIO.IN)
 
 try:
     while True:
-        output = GPIO.input(13)
-        if output != 0:
+        input_value = GPIO.input(13)
+        if input_value == False:
             print('Hello World')
+            while input_value == False:
+                input_value = GPIO.input(13)
         else:
             pass
 except KeyboardInterrupt:
