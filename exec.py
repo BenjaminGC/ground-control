a4654ec25caec8dc203dfb29b322e9f935893afc
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-07 16:45:34 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,14 +1,29 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
 
-def button_callback(channel):
-    print("Button was pushed!")
+status = False
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 13 to be an input pin and set initial value to be pulled low (off)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(13,GPIO.RISING,callback=button_callback) # Setup event on pin 13 rising edge
 
-message = input("Press enter to quit\n\n") # Run until someone presses enter
+def button_callback(status):
+    print("Status: {}".format(status))
+    if status == False:
+        status = True
+    elif status == True:
+        status = False
+    print("Status changed to: {}".format(status))
+    return status
+    
 
-GPIO.cleanup() # Clean up
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+GPIO.add_event_detect(13,GPIO.RISING,callback=exec)
+
+message = input("Press enter to quit\n\n")
+
+GPIO.cleanup()
