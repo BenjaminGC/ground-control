a97fc3551b9d88a2a309e25f04e7f4bdafbc9177
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 16:27:39 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 
 LED = 13        # GPIO27
 BUTTON = 11     # GPIO17
-status = True
+status = False
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button
@@ -11,15 +11,14 @@ GPIO.setup(LED, GPIO.OUT)  # led
 
 try:
     GPIO.output(LED, status)
-    while status:
+    while not status:
         button_input = not GPIO.input(BUTTON)
         if button_input:
-            status = False
+            status = True
     GPIO.output(LED, status)
     print(status)
     GPIO.cleanup()
 except KeyboardInterrupt:
     GPIO.cleanup()
 
-
-
+# https://github.com/BenjaminGC/ground-control.git
