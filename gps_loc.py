4a0a620d50a1ff4097fcd90961cb7d2a022c087b
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 14:35:23 +0100

    Update gps_loc.py

--- gps_loc.py
+++ gps_loc.py
@@ -4,9 +4,11 @@ import subprocess as sp
 
 gps = serial.Serial("/dev/ttyUSB0", baudrate=4800, timeout=5)
 status = True
+index = 0.0
 
 
 def gps_loc(gps):
+    global index
     line = str(gps.readline(), 'ASCII')
     data = line.split(",")
     if data[0] == '$GPRMC':
@@ -29,8 +31,9 @@ def gps_loc(gps):
             lon = long_deg + (long_min / 60)
 
             sp.call('clear', shell=True)
-            print("LAT: {}\nLON: {}".format(lat, lon))
+            print("{}: LAT= {}, LON= {}".format(index, lat, lon))
             time.sleep(0.1)
+            index += 0.1
 
 
 while status:
