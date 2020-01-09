f6270ef1622f95a92fe3b0826154d1de21d32bc8
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 23:50:36 +0100

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,6 +1,8 @@
 import csv
 
-with open('speed.csv', 'r') as csv_file:
+file = str(input('Filename: '))
+
+with open(file, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     for line in csv_reader:
