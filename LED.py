842b1edf27d4ea99db2e2ba5e2465b74f0beb91d
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 15:26:12 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,23 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 
-LED = 15
+LED = 11
 BUTTON = 13
 
-GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(LED, GPIO.OUT)
-GPIO.output(LED, False)
+GPIO.setup(BUTTON, GPIO.IN)  # button
+GPIO.setup(LED, GPIO.OUT)  # led
+GPIO.output(LED, False)     # LED begin state to false
 
-class Push:
-    @staticmethod
-    def button_callback():
-        global LED, BUTTON
+
+try:
+    while True:
+        # Turn LED off
+        print("LED off")
+        GPIO.output(LED, GPIO.LOW)
+    
+        # waiting for button press
+        while GPIO.input(BUTTON) == 1:
+            time.sleep(0.2)
+    
+            # Turn LED on
+        print("LED on")
         GPIO.output(LED, GPIO.HIGH)
-
-
-GPIO.add_event_detect(BUTTON, GPIO.RISING, callback=Push.button_callback)
-
-message = input("Press enter to quit...\n\n")
-GPIO.cleanup()
+    
+        # waiting for button release
+        while GPIO.input(BUTTON) == 0:
+            time.sleep(0.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
