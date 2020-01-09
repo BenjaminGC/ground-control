48a3d9809f77a101b82b911ca4cb38069f11c573
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 18:09:01 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -12,18 +12,22 @@ GPIO.setup(LED, GPIO.OUT)  # led
 
 
 try:
-    while True:
-        GPIO.output(LED, status)
-        while not status:
-            button_input = not GPIO.input(BUTTON)
-            if button_input and not status:
-                status = True
-            elif button_input and status:
-                status = False
-        GPIO.output(LED, status)
-        print(status)
-        sp.call('clear', shell=True)
+    while not status:
+        if not status:
+            GPIO.output(LED, status)
+            while not status:
+                button_input = not bool(GPIO.input(BUTTON))
+                if button_input and not status:
+                    status = True
+    while not status:
+        if status:
+            GPIO.output(LED, status)
+            while status:
+                button_input = not bool(GPIO.input(BUTTON))
+                if button_input and not status:
+                    status = False
 except KeyboardInterrupt:
     GPIO.cleanup()
+    print('END')
 
 # https://github.com/BenjaminGC/ground-control.git
