4a39fb3cb967d87dcdbde964e7fade5c209df68b
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 16:04:38 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+import subprocess as sp
 
 LED = 13        # GPIO27
 BUTTON = 11     # GPIO17
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(BUTTON, GPIO.IN) #, pull_up_down=GPIO.PUD_UP)  # button
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button
 GPIO.setup(LED, GPIO.OUT)  # led
 
 try:
@@ -13,7 +14,7 @@ try:
         button_input = GPIO.input(BUTTON)
         GPIO.output(LED, button_input)
         print(button_input)
-        #time.sleep(1)
+        sp.shell('clear', shell=True)       
 except KeyboardInterrupt:
     print("ENDING")
     GPIO.cleanup()
