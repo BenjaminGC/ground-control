3c6505777812a98995a8f6c5f9be973c7ed74cc8
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 16:25:53 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 
 LED = 13        # GPIO27
 BUTTON = 11     # GPIO17
-status = False
+status = True
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button
@@ -10,11 +10,16 @@ GPIO.setup(LED, GPIO.OUT)  # led
 
 
 try:
+    GPIO.output(LED, status)
+    while status:
+        button_input = not GPIO.input(BUTTON)
+        if button_input:
+            status = False
+    GPIO.output(LED, status)
     print(status)
-    button_input = not GPIO.input(BUTTON)
-    if button_input:
-        status = True
-    print(status)
-    message = input("Press enter to quit...\n\n")
+    GPIO.cleanup()
 except KeyboardInterrupt:
     GPIO.cleanup()
+
+
+
