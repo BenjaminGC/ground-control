11288455196165aca009a19e89693d434a5eda7b
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 15:56:15 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -4,7 +4,7 @@ import time
 LED = 13        # GPIO27
 BUTTON = 11     # GPIO17
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button
 GPIO.setup(LED, GPIO.OUT)  # led
 
