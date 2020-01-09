d76bffa01f6931efce2d97146ea01dc90046e65d
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 19:15:45 +0100

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,13 @@
+# Add GPIO in-/outputs to file
+
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)   # button
+GPIO.setup(LED_RED, GPIO.OUT)                           # Red led
+GPIO.setup(LED_GREEN, GPIO.OUT)                         # Green led
 GPIO.setwarnings(False)
+
 print("Cleaning up GPIO pins...")
 GPIO.cleanup([11, 13, 15])
