7170d2b433d865f341f5d0035398ca5f20fb54a0
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 14:18:55 +0100

    Update gps_loc.py

--- gps_loc.py
+++ gps_loc.py
@@ -1,6 +1,6 @@
 import serial
-import subprocess as sp
 import time
+import subprocess as sp
 
 gps = serial.Serial("/dev/ttyUSB0", baudrate=4800, timeout=5)
 status = True
@@ -11,38 +11,31 @@ def gps_loc(gps):
     data = line.split(",")
     if data[0] == '$GPRMC':
         if data[2] == 'A':
-    
+
             lat_gps = float(data[3])
             if data[4] == "S":
                 lat_gps = -lat_gps
-    
+
             lat_deg = int(lat_gps / 100)
             lat_min = lat_gps - lat_deg * 100
             lat = lat_deg + (lat_min / 60)
-    
+
             long_gps = float(data[5])
             if data[4] == "W":
                 long_gps = - long_gps
-    
+
             long_deg = int(long_gps / 100)
             long_min = long_gps - long_deg * 100
             lon = long_deg + (long_min / 60)
-    
-            return lat, lon
-
-
-def run():
-    while status:
-        try:
-            lat, lon = gps_loc(gps)
-            return lat, lon
-        except UnicodeDecodeError:
-            lat, lon = gps_loc(gps)
-            return lat, lon
-
+            
+            time.sleep(0.5)
+            print("LAT: {}\nLON: {}".foramt(lat, lon))
+            time.sleep(0.5)
+            sp.call('clear', shell=True)
+            
 
 while status:
-    lat, lon = run()
-    print("LAT: {}\nLON: {}".foramt(lat, lon))
-    time.sleep(1)
-    sp.call('clear', shell=True)
+    try:
+        gps_loc(gps)
+    except UnicodeDecodeError:
+        gps_loc(gps)
