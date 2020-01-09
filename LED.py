7dc8302cef1ae50b63d01cba73b75a4c49d59328
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 15:57:16 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -5,7 +5,7 @@ LED = 13        # GPIO27
 BUTTON = 11     # GPIO17
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # button
 GPIO.setup(LED, GPIO.OUT)  # led
 
 try:
