2a2c71720d69821110009db18b55ff00b72513a1
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-09 14:15:26 +0100

    Update gps_loc.py

--- gps_loc.py
+++ gps_loc.py
@@ -11,32 +11,38 @@ def gps_loc(gps):
     data = line.split(",")
     if data[0] == '$GPRMC':
         if data[2] == 'A':
-
+    
             lat_gps = float(data[3])
             if data[4] == "S":
                 lat_gps = -lat_gps
-
-            lat_deg = int(lat_gps/100)
-            lat_min = lat_gps - lat_deg*100
-            lat = lat_deg + (lat_min/60)
-
+    
+            lat_deg = int(lat_gps / 100)
+            lat_min = lat_gps - lat_deg * 100
+            lat = lat_deg + (lat_min / 60)
+    
             long_gps = float(data[5])
             if data[4] == "W":
                 long_gps = - long_gps
-
-            long_deg = int(long_gps/100)
-            long_min = long_gps - long_deg*100
-            lon = long_deg + (long_min/60)
-
-            print("lat: {}".format(lat))
-            print("lon: {}".format(lon))
-            
-            time.sleep(1)
-            sp.call('clear', shell=True)
     
+            long_deg = int(long_gps / 100)
+            long_min = long_gps - long_deg * 100
+            lon = long_deg + (long_min / 60)
+    
+            return lat, lon
+
+
+def run():
+    while status:
+        try:
+            lat, lon = gps_loc(gps)
+            return lat, lon
+        except UnicodeDecodeError:
+            lat, lon = gps_loc(gps)
+            return lat, lon
+
 
 while status:
-    try:
-        gps_loc(gps)
-    except UnicodeDecodeError:
-        gps_loc(gps)
+    lat, lon = run()
+    print("LAT: {}\nLON: {}".foramt(lat, lon))
+    time.sleep(1)
+    sp.call('clear', shell=True)
