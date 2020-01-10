6d5aa2c5bdb7a06541d190341304de00b50752d8
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-10 02:32:05 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -4,17 +4,35 @@ import subprocess as sp
 
 LED = 13        # GPIO27
 BUTTON = 11     # GPIO17
+status = False
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button
 GPIO.setup(LED, GPIO.OUT)  # led
 
+
 try:
-    while True:
-        button_input = not GPIO.input(BUTTON)
-        GPIO.output(LED, button_input)
-        print(button_input)
-        sp.call('clear', shell=True)       
-except KeyboardInterrupt:
-    print("ENDING")
+    if not status:
+        GPIO.output(LED, status)
+        while not status:
+            button_input = not bool(GPIO.input(BUTTON))
+            if button_input and not status:
+                status = True
+                print("LED ON")
+                time.sleep(1)
+    print(status)
+    GPIO.output(LED, status)
+    while status:
+        button_input = not bool(GPIO.input(BUTTON))
+        if button_input and status:
+            status = False
+            print("LED OFF")
+            time.sleep(1)
+    print(status)
+    GPIO.output(LED, status)
     GPIO.cleanup()
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print('FORCE QUIT')
+
