4a28ed8fcd136671e40698717f86c6a3448bdd7b
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-13 19:00:59 +0100

    Update conn_gps.py

--- conn_gps.py
+++ conn_gps.py
@@ -7,24 +7,27 @@ satellites = {}
 
 def sat_data(data):
     global satellites
-    line = str(gps.readline(), 'ASCII')
-    data = line.split(',')
-    if data[0] == '$GPGSV':
-        checksum_data = data[-1].split('*')[1].split('\r')[0]
-        data[-1] = data[-1].split('*')[0]
-        data.append(checksum_data)
-        data = data[4:-1]
-        n_sats = int(len(data)/4)
-        for i in range(n_sats):
-            name = int(data[i*4])
-            name_data = data[i*4:(i*4)+4][1:]
-            for e in range(len(name_data)):
-                if name_data[e] == '':                  # if SNR = '' => sat is out of sight but connected
-                    name_data[e] = 0
-                else:                                   # make all other values integers
-                    name_data[e] = int(name_data[e])
-            satellites[name] = name_data                # Creating satellite dictionary
-        return satellites
+    try:
+        line = str(gps.readline(), 'ASCII')
+        data = line.split(',')
+        if data[0] == '$GPGSV':
+            checksum_data = data[-1].split('*')[1].split('\r')[0]
+            data[-1] = data[-1].split('*')[0]
+            data.append(checksum_data)
+            data = data[4:-1]
+            n_sats = int(len(data)/4)
+            for i in range(n_sats):
+                name = int(data[i*4])
+                name_data = data[i*4:(i*4)+4][1:]
+                for e in range(len(name_data)):
+                    if name_data[e] == '':                  # if SNR = '' => sat is out of sight but connected
+                        name_data[e] = 0
+                    else:                                   # make all other values integers
+                        name_data[e] = int(name_data[e])
+                satellites[name] = name_data                # Creating satellite dictionary
+            return satellites
+        except UnicodeError:
+            pass
 
 
 while True:
