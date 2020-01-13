6dd4ffa19b10a710ee4d78bb1676f1b09d97a3b4
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-13 20:27:04 +0100

    Update conn_gps.py

--- conn_gps.py
+++ conn_gps.py
@@ -19,7 +19,7 @@ def sat_data(data):
             n_sats = int(len(data)/4)
             for i in range(n_sats):
                 name = int(data[i*4])
-                name_data = data[i*4:(i*4)+4][1:]
+                name_data = data[i*4:(i*4)+4]
                 for e in range(len(name_data)):
                     if name_data[e] == '':                  # if SNR = '' => sat is out of sight but connected
                         name_data[e] = 0
@@ -37,7 +37,7 @@ while True:
         sp.call('clear', shell=True)
         print("{}:{}:{}".format(time.localtime()[3], time.localtime()[4], time.localtime()[5]))
         for key, value in satellites.items():
-            print("Satellite {}: elevation = {}, azimuth = {}, SNR = {}".format(key, value[0], value[1], value[2]))
+            print("Satellite {}: elevation = {}, azimuth = {}, SNR = {}".format(key, value[1], value[2], value[3]))
         # sp.call('clear', shell=True)
     except KeyboardInterrupt:
         break
