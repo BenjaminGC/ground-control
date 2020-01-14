f8b03af5066808748cfc142923851cfb715a25f6
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-01-14 01:45:09 +0100

    Update sat_sampler.py

--- sat_sampler.py
+++ sat_sampler.py
@@ -45,7 +45,7 @@ while True:
         for key, value in satellites.items():
             samples.writerow([key, value])
         samples.writerow("-")
-        time.sleep(10)
+        time.sleep(60)
     except KeyboardInterrupt:
         break
 
